96e3a16eb5a5f94a0fd157840361997fcfaeb26f
Huubvdent <62891060+Huubvdent@users.noreply.github.com>
2023-10-03 21:23:52 +0200

    bugfix

--- src/utils/VAE.cpp
+++ src/utils/VAE.cpp
@@ -3,69 +3,60 @@
 #include <torch/script.h>
 #include <torch/torch.h>
 
-struct VarEncoder : torch::nn::Module 
-{
-    VarEncoder () 
+VarEncoder::VarEncoder () 
       : fc1(register_module("fc1", torch::nn::Linear(13, 10))),
         fc2(register_module("fc2", torch::nn::Linear(10, 6))),
         fc3(register_module("fc3", torch::nn::Linear(6, 2))),
         fc4(register_module("fc4", torch::nn::Linear(6, 2)))
-    {}
+{}
 
-    at::Tensor forward(at::Tensor x, at::Tensor sobol_tensor) 
-    {
-        x = torch::relu(fc1->forward(x));
-        x = torch::relu(fc2->forward(x));
-        mu = torch::relu(fc3->forward(x));
-        sigma = torch::relu(fc4->forward(x));
-        // Use sobol sequence to sample deterministically
-        z = mu + sigma * sobol_tensor;
-        return z;
-    }
+at::Tensor VarEncoder::forward(at::Tensor x, at::Tensor sobol_tensor) 
+{
+    x = torch::relu(fc1->forward(x));
+    x = torch::relu(fc2->forward(x));
+    mu = torch::relu(fc3->forward(x));
+    sigma = torch::relu(fc4->forward(x));
+    // Use sobol sequence to sample deterministically
+    z = mu + sigma * sobol_tensor;
+    return z;
+}
 
-    void load_weights(at::Tensor encoder_1_weight, at::Tensor encoder_2_weight, at::Tensor encoder_3_weight, at::Tensor encoder_4_weight, at::Tensor encoder_1_bias, at::Tensor encoder_2_bias, at::Tensor encoder_3_bias, at::Tensor encoder_4_bias)
-    {
-        fc1->weight = encoder_1_weight;
-        fc2->weight = encoder_2_weight;
-        fc3->weight = encoder_3_weight;
-        fc4->weight = encoder_4_weight;
+void VarEncoder::load_weights(at::Tensor encoder_1_weight, at::Tensor encoder_2_weight, at::Tensor encoder_3_weight, at::Tensor encoder_4_weight, at::Tensor encoder_1_bias, at::Tensor encoder_2_bias, at::Tensor encoder_3_bias, at::Tensor encoder_4_bias)
+{
+    fc1->weight = encoder_1_weight;
+    fc2->weight = encoder_2_weight;
+    fc3->weight = encoder_3_weight;
+    fc4->weight = encoder_4_weight;
 
-        fc1->bias = encoder_1_bias;
-        fc2->bias = encoder_2_bias;
-        fc3->bias = encoder_3_bias;
-        fc4->bias = encoder_4_bias;
-    }
+    fc1->bias = encoder_1_bias;
+    fc2->bias = encoder_2_bias;
+    fc3->bias = encoder_3_bias;
+    fc4->bias = encoder_4_bias;
+}
 
-    torch::nn::Linear fc1, fc2, fc3, fc4;
 
-};
 
-struct Decoder : torch::nn::Module {
-    Decoder () 
+Decoder::Decoder () 
         : fc1(register_module("fc1", torch::nn::Linear(2, 6))),
           fc2(register_module("fc2", torch::nn::Linear(6, 10))),
           fc3(register_module("fc3", torch::nn::Linear(10, 13)))
-    {}
+{}
 
-    at::Tensor forward(at::Tensor x) 
-    {
-        x = torch::relu(fc1->forward(x));
-        x = torch::relu(fc2->forward(x));
-        x = torch::sigmoid(fc3->forward(x));
-        return x;
-    }
+at::Tensor Decoder::forward(at::Tensor x) 
+{
+    x = torch::relu(fc1->forward(x));
+    x = torch::relu(fc2->forward(x));
+    x = torch::sigmoid(fc3->forward(x));
+    return x;
+}
 
-    void load_weights(at::Tensor decoder_1_weight, at::Tensor decoder_2_weight, at::Tensor decoder_3_weight, at::Tensor decoder_1_bias, at::Tensor decoder_2_bias, at::Tensor decoder_3_bias)
-    {
-        fc1->weight = decoder_1_weight;
-        fc2->weight = decoder_2_weight;
-        fc3->weight = decoder_3_weight;
+void Decoder::load_weights(at::Tensor decoder_1_weight, at::Tensor decoder_2_weight, at::Tensor decoder_3_weight, at::Tensor decoder_1_bias, at::Tensor decoder_2_bias, at::Tensor decoder_3_bias)
+{
+    fc1->weight = decoder_1_weight;
+    fc2->weight = decoder_2_weight;
+    fc3->weight = decoder_3_weight;
 
-        fc1->bias = decoder_1_bias;
-        fc2->bias = decoder_2_bias;
-        fc3->bias = decoder_3_bias;
-    }
-
-    torch::nn::Linear fc1, fc2, fc3;
-
-};
+    fc1->bias = decoder_1_bias;
+    fc2->bias = decoder_2_bias;
+    fc3->bias = decoder_3_bias;
+}
