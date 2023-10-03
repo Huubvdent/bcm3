ab80c6557109271079a26b95ce9e9050337313d2
Huubvdent <62891060+Huubvdent@users.noreply.github.com>
2023-10-03 15:42:37 +0200

    bugfix

--- src/utils/VAE.h
+++ src/utils/VAE.h
@@ -3,19 +3,19 @@
 #include <torch/script.h>
 #include <torch/torch.h>
 
-struct VarEncoder : torch::jit::nn::Module {
+struct VarEncoder : torch::nn::Module {
     VarEncoder () {};
     torch::Tensor forward(torch::Tensor x, torch::Tensor sobol_tensor);
-    torch::jit::nn::Linear fc1;
-    torch::jit::nn::Linear fc2;
-    torch::jit::nn::Linear fc3;
-    torch::jit::nn::Linear fc4;
+    torch::nn::Linear fc1;
+    torch::nn::Linear fc2;
+    torch::nn::Linear fc3;
+    torch::nn::Linear fc4;
 };
 
-struct Decoder : torch::jit::nn::Module {
+struct Decoder : torch::nn::Module {
     Decoder () {};
     torch::Tensor forward(torch::Tensor x, torch::Tensor sobol_tensor) {};
-    torch::jit::nn::Linear fc1;
-    torch::jit::nn::Linear fc2;
-    torch::jit::nn::Linear fc3;
+    torch::nn::Linear fc1;
+    torch::nn::Linear fc2;
+    torch::nn::Linear fc3;
 };
