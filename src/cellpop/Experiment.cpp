4dffa038d5a0b9bbc7b1d2077bbc253e806e9c9b
Huubvdent <62891060+Huubvdent@users.noreply.github.com>
2023-10-04 16:25:28 +0200

    logging

--- src/cellpop/Experiment.cpp
+++ src/cellpop/Experiment.cpp
@@ -1194,6 +1194,21 @@ size_t Experiment::AddNewCell(Real time, Cell* parent, const VectorReal& transfo
 	at::Tensor max = container.attr("max").toTensor();
 
 	at::Tensor encoder_1_weight = container.attr("encoder_1_weight").toTensor();
+
+	// Get the sizes of all dimensions
+    c10::IntArrayRef sizes = encoder_1_weight.sizes();
+
+    // Log the sizes of all dimensions
+    std::string logMessage = "Tensor Sizes: ";
+    for (size_t i = 0; i < sizes.size(); ++i) {
+        logMessage += std::to_string(sizes[i]);
+        if (i < sizes.size() - 1) {
+            logMessage += "x";
+        }
+    }
+
+	BCMLOG(logMessage.c_str());
+
 	at::Tensor encoder_2_weight = container.attr("encoder_2_weight").toTensor();
 	at::Tensor encoder_3_weight = container.attr("encoder_3_weight").toTensor();
 	at::Tensor encoder_4_weight = container.attr("encoder_4_weight").toTensor();
