dd069f037fbb8ead4a141fb630cb9912ff4d5493
Huubvdent <62891060+Huubvdent@users.noreply.github.com>
2023-10-10 18:16:17 +0200

    log2

--- src/cellpop/Cell.cpp
+++ src/cellpop/Cell.cpp
@@ -276,7 +276,7 @@ bool Cell::Initialize(Real creation_time, const VectorReal& transformed_variable
 
 	at::Tensor log2scaled_input = torch::log2(input_tensor);
 
-	at::Tensor complete = log2scaled_input * torch::pow((torch::ones(n-5,torch::kFloat32) * 2), variance_tensor);
+	at::Tensor complete = log2scaled_input * torch::pow((torch::ones(n-3,torch::kFloat32) * 2), variance_tensor);
 
 	at::Tensor rescaled = torch::pow((torch::ones(n-5,torch::kFloat32) * 2), complete);
 
